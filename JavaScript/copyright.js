60b96f73105861ff639b59cb64dc3647ddcc6a83
Julie Possemiers <JULIE.POSSEMIERS@vdabcampus.be>
2024-12-01 16:57:55 +0100

    copyright

---
+++ JavaScript/copyright.js
@@ -0,0 +1,6 @@
+"use strict";
+const vdabHyperlink = document.getElementById("vdab");
+console.log(vdabHyperlink);
+console.log(vdabHyperlink.href);
+console.log(vdabHyperlink.innerText);
+document.getElementById("jaar").innerText = new Date().getFullYear();
