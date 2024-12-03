b5ab857f260a82248d1af8ee23423855c97802a8
JulieVD84 <JULIE.POSSEMIERS@vdab.campus.be>
2024-12-03 14:39:43 +0100

    blad steen schaar : laatste versie

--- JavaScript/bladsteenschaar/bladsteenschaar.js
+++ JavaScript/bladsteenschaar/bladsteenschaar.js
@@ -9,15 +9,12 @@ for (const selectedImage of document.querySelectorAll("img.selecteer")) {
         let gelijkSpel = false;
 
         // Bereken Keuze
-        // 1 = Blad
-        // 2 = Steen
-        // 3 = Schaar
+        // 1 = Blad 2 = Steen 3 = Schaar
         let keuze = Math.floor((Math.random() * 3) + 1);
 
         // Keuzemogelijkheden
         switch (this.id) {
 
-            // Gebruiker Blad
             case "blad":
                 if (keuze === 1) {
                     gelijkSpel = true;
@@ -27,7 +24,6 @@ for (const selectedImage of document.querySelectorAll("img.selecteer")) {
                 }
                 break;
 
-            // Gebruiker Steen
             case "steen":
                 if (keuze === 2) {
                     gelijkSpel = true;
@@ -37,7 +33,6 @@ for (const selectedImage of document.querySelectorAll("img.selecteer")) {
                 }
                 break;
 
-            // Gebruiker Schaar
             case "schaar":
                 if (keuze === 3) {
                     gelijkSpel = true;
@@ -49,30 +44,20 @@ for (const selectedImage of document.querySelectorAll("img.selecteer")) {
         }
 
         // Toon Computerkeuze
-        const computerKeuze = document.getElementById("keuze");
-
         switch (keuze) {
             case 1:
-                computerKeuze.src = "blad.png"
+                document.getElementById("keuze").src = "blad.png"
                 break;
             case 2:
-                computerKeuze.src = "steen.png"
+                document.getElementById("keuze").src = "steen.png"
                 break;
             case 3:
-                computerKeuze.src = "schaar.png"
+                document.getElementById("keuze").src = "schaar.png"
                 break;
         }
         document.getElementById("keuze").hidden = false;
 
         // Toon Winnaar
-        if (gelijkSpel) {
-            document.getElementById("resultaat").innerText = "Winnaar : Gelijkspel";
-        } else {
-            if (gewonnen) {
-                document.getElementById("resultaat").innerText = "Winnaar : Gebruiker";
-            } else {
-                document.getElementById("resultaat").innerText = "Winnaar : Computer";
-            }
-        }
+        document.getElementById("resultaat").innerText = gelijkSpel === true ? "Winnaar : Gelijkspel" : gewonnen === true ? "Winnaar : Gebruiker" : "Winnaar : Computer";
     }
 }
