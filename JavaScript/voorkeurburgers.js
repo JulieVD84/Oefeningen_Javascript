d1abc415328588be04a13f3ee1183e744fea4f7a
JulieVD84 <JULIE.POSSEMIERS@vdab.campus.be>
2024-12-05 15:43:38 +0100

    voorkeurburgers

--- JavaScript/voorkeurburgers.js
+++ JavaScript/voorkeurburgers.js
@@ -21,3 +21,70 @@ allesNaarRechtsButton.onclick = function () {
     this.disabled = true;
     allesNaarLinksButton.disabled = false;
 };
+
+
+
+
+function verplaatsAlleBurgersVanNaar(vanSelect, naarSelect) {
+    for (const vanOption of vanSelect.options) {
+        // option maken in de select naarwaar je de options kopieert:
+        const naarOption = document.createElement("option");
+        naarOption.innerText = vanOption.innerText;
+        naarSelect.appendChild(naarOption);
+    }
+    // de options verwijderen in de select vanwaar je de options kopieerde
+    vanSelect.options.length = 0;
+}
+allesNaarRechtsButton.onclick = function () {
+    verplaatsAlleBurgersVanNaar(linksSelect, rechtsSelect);
+    this.disabled = true;
+    allesNaarLinksButton.disabled = false;
+};
+allesNaarLinksButton.onclick = function () {
+    verplaatsAlleBurgersVanNaar(rechtsSelect, linksSelect);
+    this.disabled = true;
+    allesNaarRechtsButton.disabled = false;
+};
+
+
+
+eenNaarRechtsButton.onclick = function () {
+    const gekozenBurger = linksSelect.value;
+    const option = document.createElement("option");
+    option.innerText = gekozenBurger;
+    rechtsSelect.appendChild(option);
+    linksSelect.remove(linksSelect.selectedIndex);
+    this.disabled = true;
+    if (linksSelect.length === 0) {
+        allesNaarRechtsButton.disabled = true;
+    }
+    allesNaarLinksButton.disabled = false;
+};
+
+
+
+function verplaatsEenBurgerVanNaar(vanSelect, naarSelect) {
+    const naam = vanSelect.value;
+    const option = document.createElement("option");
+    option.innerText = naam;
+    naarSelect.appendChild(option);
+    vanSelect.remove(vanSelect.selectedIndex);
+}
+eenNaarRechtsButton.onclick = function () {
+    verplaatsEenBurgerVanNaar(linksSelect, rechtsSelect);
+    this.disabled = true;
+    if (linksSelect.length === 0) {
+        allesNaarRechtsButton.disabled = true;
+    }
+    allesNaarLinksButton.disabled = false;
+};
+eenNaarLinksButton.onclick = function () {
+    verplaatsEenBurgerVanNaar(rechtsSelect, linksSelect);
+    this.disabled = true;
+    if (rechtsSelect.length === 0) {
+        allesNaarLinksButton.disabled = true;
+    }
+    allesNaarRechtsButton.disabled = false;
+};
+
+
